f1b901f6fd6fe74b616cd4a57bdbb8f8215f2a2d
Marcio <marciopivajunior457@gmail.com>
2023-12-03 04:00:39 -0300

    Refact: avaliacoes

--- src/components/Assessment/index.jsx
+++ src/components/Assessment/index.jsx
@@ -1,9 +1,9 @@
 import Card from "./Card";
 import styles from "./style.module.scss";
 
-import personImg from "../../assets/imgs/old_woman_home.jpg";
-
-import { Swiper, SwiperSlide } from "swiper/react";
+import { Swiper, SwiperSlide} from "swiper/react";
+import { Navigation } from 'swiper/modules';
+import 'swiper/css/navigation';
 import "swiper/css";
 import { getDatabase, onValue, ref } from "firebase/database";
 import { useEffect, useState } from "react";
@@ -25,7 +25,7 @@ export default function Assessment() {
       let assessmentsBD = [];
       let txtAssesmentsBD = [];
 
-      arrayDatas.forEach((e,i) => {
+      arrayDatas.forEach((e) => {
         namesBD.push(e[1].userAssesment[0])
         photoProfilesBD.push(e[1].userAssesment[1])
         assessmentsBD.push(e[1].userAssesment[2])
@@ -47,15 +47,17 @@ export default function Assessment() {
   return (
     <section className={styles.assesssment__content} id="assessment">
       <h1 onClick={dataAssesments}>Avaliações</h1>
-      <Swiper spaceBetween={50} slidesPerView={3}>
+      <Swiper navigation={true} modules={[Navigation]} spaceBetween={50} slidesPerView={1} breakpoints={{820: {slidesPerView:2},1120: {slidesPerView:3}}}>
         {names.map((e, i) => (
-          <Card
-            key={i}
-            personName={names[i]}
-            personImg={photoProfiles[i]}
-            asses={assessments[i]}
-            txtAssess={txtAssessments[i]}
-          ></Card>
+          <SwiperSlide key={i}>
+            <Card
+              key={i}
+              personName={names[i]}
+              personImg={photoProfiles[i]}
+              asses={assessments[i]}
+              txtAssess={txtAssessments[i]}
+            ></Card>
+          </SwiperSlide>
         ))}
       </Swiper>
     </section>
--- src/components/DashboardAssessment/index.jsx
+++ src/components/DashboardAssessment/index.jsx
@@ -1,124 +1,161 @@
 /* eslint-disable default-case */
 // Importe os módulos do Firebase
-import DashboardTop from 'components/DashboardTop';
-import Footer from 'components/Footer';
-import Header from 'components/Header';
-import {  getDatabase, off, onValue, ref, set, update } from 'firebase/database';
-import { app } from 'services/firebaseConfig';
-import Star from './Star';
-import { useState } from 'react';
-import { getAuth, onAuthStateChanged } from 'firebase/auth';
-import { doc, getDoc, getFirestore } from 'firebase/firestore';
-  export default function DashboardAssessment(){
-    const items = [...(new Array(5).keys())];
-    const [activeIndex, setActiveIndex] = useState(null);
-    let submitBdAssessment = 0;
+import DashboardTop from "components/DashboardTop";
+import Footer from "components/Footer";
+import Header from "components/Header";
+import { getDatabase, off, onValue, ref, set, update } from "firebase/database";
+import { app } from "services/firebaseConfig";
+import Star from "./Star";
+import { useEffect, useState } from "react";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { doc, getDoc, getFirestore } from "firebase/firestore";
 
-    switch (activeIndex) {
-      case undefined:
-        submitBdAssessment = 0
-        break;
-      case 0:
-        submitBdAssessment = 1
-        break;
-      case 1:
-        submitBdAssessment = 2
-        break;
-      case 2:
-        submitBdAssessment = 3
-        break;
-      case 3:
-        submitBdAssessment = 4
-        break;
-      case 4:
-        submitBdAssessment = 5
-        break;
-    }
-  
-    const onClickStar = (index) => {
-      setActiveIndex((oldState) => (oldState === index ? undefined : index));
-    };
+import styles from "./style.module.scss";
+import { useNavigate } from "react-router-dom";
+export default function DashboardAssessment() {
+  const items = [...new Array(5).keys()];
+  const [activeIndex, setActiveIndex] = useState(null);
+  let submitBdAssessment = 0;
 
+  switch (activeIndex) {
+    case undefined:
+      submitBdAssessment = 0;
+      break;
+    case 0:
+      submitBdAssessment = 1;
+      break;
+    case 1:
+      submitBdAssessment = 2;
+      break;
+    case 2:
+      submitBdAssessment = 3;
+      break;
+    case 3:
+      submitBdAssessment = 4;
+      break;
+    case 4:
+      submitBdAssessment = 5;
+      break;
+  }
 
-    const [name,setName] = useState();
-    const [photoProfile, setPhotoProfile] = useState();
-    const [txtAssessment, setTxtAssessment] = useState('');
-    const handleTxt = (e) => {
-        setTxtAssessment(e)
-    }
-    const auth = getAuth(app);
-    const db = getFirestore();
-    const unsubscribe = onAuthStateChanged(auth, async (user) => {
-        if (user) {
-            const userId = user.uid;
+  const onClickStar = (index) => {
+    setActiveIndex((oldState) => (oldState === index ? undefined : index));
+  };
 
-            const userDocRef = doc(db, 'users', userId);
+  const [name, setName] = useState();
+  const [photoProfile, setPhotoProfile] = useState();
+  const [txtAssessment, setTxtAssessment] = useState("");
+  const [isAssessment, setIsAssessment] = useState(null);
+  const handleTxt = (e) => {
+    setTxtAssessment(e);
+  };
+  const auth = getAuth(app);
+  const db = getFirestore();
+  const unsubscribe = onAuthStateChanged(auth, async (user) => {
+    if (user) {
+      const userId = user.uid;
 
-            try {
-                const userDocSnapshot = await getDoc(userDocRef);
+      const userDocRef = doc(db, "users", userId);
 
-                if (userDocSnapshot.exists()) {
-                    const userData = userDocSnapshot.data();
-                    setName( userData.nome)
-                    setPhotoProfile(userData.fotoperfil)
-                }
-            } catch (error) {
-            }
+      try {
+        const userDocSnapshot = await getDoc(userDocRef);
+
+        if (userDocSnapshot.exists()) {
+          const userData = userDocSnapshot.data();
+          setName(userData.nome);
+          setPhotoProfile(userData.fotoperfil);
         }
-    });
+      } catch (error) {}
+    }
+  });
 
-    const database = getDatabase();
-    const user = auth.currentUser.uid
-    const referenceNo = ref(database, `assessments/${user}`);
+  const database = getDatabase();
+  const user = auth.currentUser.uid;
+  const referenceNo = ref(database, `assessments/${user}`);
 
-    const userAssesment = [
-      name,
-      photoProfile,
-      txtAssessment,
-      submitBdAssessment
-    ]
+  const userAssesment = [name, photoProfile, txtAssessment, submitBdAssessment];
 
-    const pushBD = () => {
-      const onDataChange = snapshot => {
-        const value = snapshot.val();
-        const existOnTheDB = value.assessments[user];
-    
-        if (existOnTheDB) {
-          console.log('alterado');
-          update(referenceNo, {
-            userAssesment
-          });
-        } else {
-          set(referenceNo, {
-            userAssesment
-          });
-        }
-        off(ref(database), 'value', onDataChange);
-      };
-      onValue(ref(database), onDataChange);
+  const pushBD = () => {
+    const onDataChange = (snapshot) => {
+      const value = snapshot.val();
+      const existOnTheDB = value.assessments[user];
+
+      if (existOnTheDB) {
+        update(referenceNo, {
+          userAssesment,
+        });
+      } else {
+        set(referenceNo, {
+          userAssesment,
+        });
+      }
+      off(ref(database), "value", onDataChange);
     };
-    
+    onValue(ref(database), onDataChange);
+    setIsAssessment(true)
+  };
 
+  const existAssesment = () => {
+    const onDataChange = (snapshot) => {
+      const value = snapshot.val();
+      const existOnTheDB = value.assessments[user];
+      if (existOnTheDB) {
+        setIsAssessment(true);
+      } else {
+        setIsAssessment(false);
+      }
+      off(ref(database), "value", onDataChange);
+    };
+    onValue(ref(database), onDataChange);
+  };
+  console.log(isAssessment)
+  const navigate = useNavigate();
+  const backPage = () => navigate(-1)
+  const remakeAssesment = () => setIsAssessment(false)
 
-    return (
-        <>
-        <Header bg='#282D35' isDashboard></Header>
-        <DashboardTop title='Avalia-nos'></DashboardTop>
-        <div>
-            <h1>Avalie nossa plataforma abaixo:</h1>
+  useEffect(() => {
+    existAssesment();
+  }, []);
+
+  return (
+    <>
+      <Header bg="#282D35" isDashboard></Header>
+      <DashboardTop title="Avalia-nos"></DashboardTop>
+      {isAssessment ? (
+        <div className={styles.assessment__container}>
+          <h1 className={styles.title__isAssessment}>Você já nos avaliou, deseja refazer a sua avaliação?</h1>
+          <div className={styles.assessment__container__buttons}>
+            <button onClick={remakeAssesment}>Refazer</button>
+            <button onClick={backPage}>Voltar</button>
+          </div>
+        </div>
+      ) : (
+        <div className={styles.assessment__container}>
+          <h1>Avalie nossa plataforma abaixo:</h1>
+          <div className={styles.assessment__container__stars}>
             {items.map((index) => (
-                <Star
+              <Star
                 onClick={() => onClickStar(index)}
                 key={`star_${index}`}
                 isActive={index <= activeIndex}
-                />
+              />
             ))}
-            <div>ESTRELAS</div>
-            <textarea placeholder='Qual foi sua experiência? (OPCIONAL)' onChange={e => handleTxt(e.target.value)}></textarea>
-            <button onClick={pushBD}> Enviar avaliação </button>
-        </div>
+          </div>
+          <textarea
+            placeholder="Qual foi sua experiência?"
+            onChange={(e) => handleTxt(e.target.value)}
+          ></textarea>
+          <button onClick={pushBD} className={styles.button__submit}>
+            Enviar avaliação
+          </button>
 
-        <Footer></Footer>
-        </>
-    )
-  }
+          <div className={styles.box__thanks}>
+            <h1>OBRIGADO PELA SUA AVALIAÇÃO!</h1>
+          </div>
+        </div>
+      )}
+
+      <Footer></Footer>
+    </>
+  );
+}
